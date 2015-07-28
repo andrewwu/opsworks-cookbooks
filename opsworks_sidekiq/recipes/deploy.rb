514e4e223e4fb79c974833c48ec71baf7ae3274c
Andrew Wu <wujuichang@gmail.com>
2015-07-27 21:36:39 -0700

    sidekiq depends on redis-config

--- opsworks_sidekiq/recipes/deploy.rb
+++ opsworks_sidekiq/recipes/deploy.rb
@@ -16,6 +16,7 @@ node[:deploy].each do |application, deploy|
   end
 
   include_recipe "opsworks_sidekiq::setup"
+  include_recipe "redis-config::generate"
 
   template "#{deploy[:deploy_to]}/shared/config/memcached.yml" do
     cookbook "rails"
