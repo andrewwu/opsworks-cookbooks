f5843af2a9ece5a131230249bf37b6247709b2da
Andrew Wu <wujuichang@gmail.com>
2015-07-29 22:50:57 -0700

    Add debug

--- opsworks_sidekiq/recipes/deploy.rb
+++ opsworks_sidekiq/recipes/deploy.rb
@@ -18,7 +18,10 @@ node[:deploy].each do |application, deploy|
   include_recipe "opsworks_sidekiq::setup"
 
   if !File.exist?("#{deploy[:deploy_to]}/shared/app.env")
+    Chef::Log.info("FILE DOESNT EXISTS LOL")
     include_recipe "opsworks_nodejs::create_env_file"
+  else
+    Chef::Log.info("FILE EXISTS LOL")
   end
 
   template "#{deploy[:deploy_to]}/shared/config/memcached.yml" do
